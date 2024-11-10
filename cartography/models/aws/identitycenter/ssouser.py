33ba7ecb50c15d69078ea80e70a466ab3f270bcc
= <=>
2024-11-10 20:24:46 +0100

    fix lint issue

--- cartography/models/aws/identitycenter/ssouser.py
+++ cartography/models/aws/identitycenter/ssouser.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
 from cartography.models.core.common import PropertyRef
-from cartography.models.core.nodes import CartographyNodeProperties, CartographyNodeSchema, ExtraNodeLabels
+from cartography.models.core.nodes import CartographyNodeProperties
+from cartography.models.core.nodes import CartographyNodeSchema
+from cartography.models.core.nodes import ExtraNodeLabels
 from cartography.models.core.relationships import CartographyRelProperties
 from cartography.models.core.relationships import CartographyRelSchema
 from cartography.models.core.relationships import LinkDirection
