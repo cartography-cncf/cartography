42656e22cefca678a26a3a0d1fa7bc1e49ba9ace
Alex Chantavy <chantavy@gmail.com>
2024-12-28 19:18:31 -0800

    refactor
    Signed-off-by: Alex Chantavy <chantavy@gmail.com>

--- cartography/models/aws/ec2/keypair.py
+++ cartography/models/aws/ec2/keypair.py
@@ -8,62 +8,47 @@ from cartography.models.core.relationships import CartographyRelProperties
 from cartography.models.core.relationships import CartographyRelSchema
 from cartography.models.core.relationships import LinkDirection
 from cartography.models.core.relationships import make_target_node_matcher
-from cartography.models.core.relationships import OtherRelationships
 from cartography.models.core.relationships import TargetNodeMatcher
 
 
 @dataclass(frozen=True)
-class EC2KeypairInstanceNodeProperties(CartographyNodeProperties):
+class EC2KeyPairNodeProperties(CartographyNodeProperties):
+    """
+    Properties for EC2 keypairs from describe-key-pairs
+    """
     id: PropertyRef = PropertyRef('KeyPairArn')
     arn: PropertyRef = PropertyRef('KeyPairArn', extra_index=True)
     keyname: PropertyRef = PropertyRef('KeyName')
+    key_fingerprint: PropertyRef = PropertyRef('KeyFingerprint')
     region: PropertyRef = PropertyRef('Region', set_in_kwargs=True)
     lastupdated: PropertyRef = PropertyRef('lastupdated', set_in_kwargs=True)
 
 
 @dataclass(frozen=True)
-class EC2KeypairInstanceToAwsAccountRelProperties(CartographyRelProperties):
+class EC2KeyPairToAwsAccountRelProperties(CartographyRelProperties):
     lastupdated: PropertyRef = PropertyRef('lastupdated', set_in_kwargs=True)
 
 
 @dataclass(frozen=True)
-class EC2KeypairInstanceToAWSAccount(CartographyRelSchema):
+class EC2KeyPairToAWSAccount(CartographyRelSchema):
+    """
+    Relationship schema for EC2 keypairs to AWS Accounts
+    """
     target_node_label: str = 'AWSAccount'
     target_node_matcher: TargetNodeMatcher = make_target_node_matcher(
         {'id': PropertyRef('AWS_ID', set_in_kwargs=True)},
     )
     direction: LinkDirection = LinkDirection.INWARD
-    rel_label: str = "RESOURCE"
-    properties: EC2KeypairInstanceToAwsAccountRelProperties = EC2KeypairInstanceToAwsAccountRelProperties()
+    rel_label: str = 'RESOURCE'
+    properties: EC2KeyPairToAwsAccountRelProperties = EC2KeyPairToAwsAccountRelProperties()
 
 
 @dataclass(frozen=True)
-class EC2KeypairInstanceToEC2InstanceRelProperties(CartographyRelProperties):
-    lastupdated: PropertyRef = PropertyRef('lastupdated', set_in_kwargs=True)
-
-
-@dataclass(frozen=True)
-class EC2KeypairInstanceToEC2Instance(CartographyRelSchema):
-    target_node_label: str = 'EC2Instance'
-    target_node_matcher: TargetNodeMatcher = make_target_node_matcher(
-        {'id': PropertyRef('InstanceId')},
-    )
-    direction: LinkDirection = LinkDirection.OUTWARD
-    rel_label: str = "SSH_LOGIN_TO"
-    properties: EC2KeypairInstanceToEC2InstanceRelProperties = EC2KeypairInstanceToEC2InstanceRelProperties()
-
-
-@dataclass(frozen=True)
-class EC2KeypairInstanceSchema(CartographyNodeSchema):
+class EC2KeyPairSchema(CartographyNodeSchema):
     """
-    EC2 keypairs as known by describe-instances.
+    Schema for EC2 keypairs from describe-key-pairs
     """
     label: str = 'EC2KeyPair'
     extra_node_labels: ExtraNodeLabels = ExtraNodeLabels(['KeyPair'])
-    properties: EC2KeypairInstanceNodeProperties = EC2KeypairInstanceNodeProperties()
-    sub_resource_relationship: EC2KeypairInstanceToAWSAccount = EC2KeypairInstanceToAWSAccount()
-    other_relationships: OtherRelationships = OtherRelationships(
-        [
-            EC2KeypairInstanceToEC2Instance(),
-        ],
-    )
+    properties: EC2KeyPairNodeProperties = EC2KeyPairNodeProperties()
+    sub_resource_relationship: EC2KeyPairToAWSAccount = EC2KeyPairToAWSAccount()
