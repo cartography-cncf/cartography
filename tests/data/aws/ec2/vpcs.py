362c78286e2a5fb839ddffaf80b465406dc4053e
Alex Chantavy <alex@subimage.io>
2025-04-17 11:28:42 -0700

    Add tests for VPCs
    Signed-off-by: Alex Chantavy <alex@subimage.io>

---
+++ tests/data/aws/ec2/vpcs.py
@@ -0,0 +1,38 @@
+TEST_VPCS = [
+    {
+        'OwnerId': '12345',
+        'InstanceTenancy': 'default',
+        'CidrBlockAssociationSet': [{
+            'AssociationId': 'vpc-cidr-assoc-0daea',
+            'CidrBlock': '172.31.0.0/16',
+            'CidrBlockState': {'State': 'associated'},
+        }],
+        'IsDefault': True,
+        'BlockPublicAccessStates': {'InternetGatewayBlockMode': 'off'},
+        'VpcId': 'vpc-038cf',
+        'State': 'available',
+        'CidrBlock': '172.31.0.0/16',
+        'DhcpOptionsId': 'dopt-036d',
+    },
+    {
+        'OwnerId': '12345',
+        'InstanceTenancy': 'default',
+        'CidrBlockAssociationSet': [{
+            'AssociationId': 'vpc-cidr-assoc-087ee',
+            'CidrBlock': '10.1.0.0/16',
+            'CidrBlockState': {'State': 'associated'},
+        }],
+        'IsDefault': False,
+        'Tags': [
+            {'Key': 'Environment', 'Value': 'staging'},
+            {'Key': 'Customer', 'Value': 'subimage'},
+            {'Key': 'Name', 'Value': 'subimage-vpc'},
+            {'Key': 'ManagedBy', 'Value': 'terraform'},
+        ],
+        'BlockPublicAccessStates': {'InternetGatewayBlockMode': 'off'},
+        'VpcId': 'vpc-0f510',
+        'State': 'available',
+        'CidrBlock': '10.1.0.0/16',
+        'DhcpOptionsId': 'dopt-036d',
+    },
+]
