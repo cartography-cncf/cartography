8da1da9445adab1fda7dac841c997486c43456a2
Jeremy Chapeau <jeremy.chapeau@gmail.com>
2025-05-15 09:50:25 +0200

    fix lastpass model
    Signed-off-by: Jeremy Chapeau <jeremy.chapeau@gmail.com>

--- cartography/models/lastpass/user.py
+++ cartography/models/lastpass/user.py
@@ -57,13 +57,13 @@ class LastpassTenantToLastpassUserRelProperties(CartographyRelProperties):
 
 
 @dataclass(frozen=True)
-# (:LastpassTenant)<-[:RESOURCE]-(:LastpassUser)
+# (:LastpassTenant)-[:RESOURCE]->(:LastpassUser)
 class LastpassTenantToUserRel(CartographyRelSchema):
     target_node_label: str = "LastpassTenant"
     target_node_matcher: TargetNodeMatcher = make_target_node_matcher(
         {"id": PropertyRef("TENANT_ID", set_in_kwargs=True)},
     )
-    direction: LinkDirection = LinkDirection.OUTWARD
+    direction: LinkDirection = LinkDirection.INWARD
     rel_label: str = "RESOURCE"
     properties: LastpassTenantToLastpassUserRelProperties = (
         LastpassTenantToLastpassUserRelProperties()
