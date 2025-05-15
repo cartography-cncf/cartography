65517815f9b42a7b5d6f973305fbe96d1981685a
Jeremy Chapeau <jeremy.chapeau@gmail.com>
2025-05-15 09:45:00 +0200

    fix digitalcoean model
    Signed-off-by: Jeremy Chapeau <jeremy.chapeau@gmail.com>

--- cartography/models/digitalocean/droplet.py
+++ cartography/models/digitalocean/droplet.py
@@ -38,13 +38,13 @@ class DODropletToAccountRelProperties(CartographyRelProperties):
 
 
 @dataclass(frozen=True)
-# (:DOProject)<-[:RESOURCE]-(:DODroplet)
+# (:DOProject)-[:RESOURCE]->(:DODroplet)
 class DODropletToAccountRel(CartographyRelSchema):
     target_node_label: str = "DOProject"
     target_node_matcher: TargetNodeMatcher = make_target_node_matcher(
         {"id": PropertyRef("PROJECT_ID", set_in_kwargs=True)},
     )
-    direction: LinkDirection = LinkDirection.OUTWARD
+    direction: LinkDirection = LinkDirection.INWARD
     rel_label: str = "RESOURCE"
     properties: DODropletToAccountRelProperties = DODropletToAccountRelProperties()
 
