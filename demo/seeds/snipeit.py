e624011c43af0361f41b3a275d6edef9e7ab4b77
Jeremy Chapeau <jeremy.chapeau@gmail.com>
2025-05-16 14:14:48 +0200

    add snipeit
    Signed-off-by: Jeremy Chapeau <jeremy.chapeau@gmail.com>

---
+++ demo/seeds/snipeit.py
@@ -0,0 +1,35 @@
+import cartography.intel.snipeit.user
+import cartography.intel.snipeit.asset
+import tests.data.snipeit.users
+import tests.data.snipeit.tenants
+import tests.data.snipeit.assets
+from demo.seeds.base import Seed
+
+
+class SnipeitSeed(Seed):
+
+
+    def seed(self, *args) -> None:
+        for tenant_name, tenant in tests.data.snipeit.tenants.TENANTS.items():
+            self._seed_users(tenant_name, tenant['id'])
+            self._seed_assets(tenant_name, tenant['id'])
+
+    def _seed_users(self, tenant_name: str, tenant_id: str) -> None:
+        cartography.intel.snipeit.user.load_users(
+            self.neo4j_session,
+            {
+                "UPDATE_TAG": self.update_tag,
+                "TENANT_ID": tenant_id,
+            },
+            tests.data.snipeit.users.USERS[tenant_name],
+        )
+
+    def _seed_assets(self, tenant_name: str, tenant_id: str) -> None:
+        cartography.intel.snipeit.asset.load_assets(
+            self.neo4j_session,
+            {
+                "UPDATE_TAG": self.update_tag,
+                "TENANT_ID": tenant_id,
+            },
+            tests.data.snipeit.assets.ASSETS[tenant_name],
+        )
