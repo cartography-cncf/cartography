c1da48585375dc1293d69e0e8a85aa7dc46487e2
AdityaPandeyCN <adityapand3y666@gmail.com>
2025-05-16 20:03:47 +0530

    modified the test file to use the mock data
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- tests/integration/cartography/intel/aws/test_sns.py
+++ tests/integration/cartography/intel/aws/test_sns.py
@@ -3,6 +3,8 @@ from unittest.mock import patch
 
 import cartography.intel.aws.sns
 from cartography.intel.aws.sns import sync
+from tests.data.aws.sns import GET_TOPIC_ATTRIBUTES
+from tests.data.aws.sns import LIST_TOPICS
 from tests.integration.cartography.intel.aws.common import create_test_account
 from tests.integration.util import check_nodes
 from tests.integration.util import check_rels
@@ -11,34 +13,20 @@ TEST_ACCOUNT_ID = "000000000000"
 TEST_REGION = "us-east-1"
 TEST_UPDATE_TAG = 123456789
 
-TEST_TOPIC_DATA = [{
-    'TopicArn': 'arn:aws:sns:us-east-1:123456789012:test-topic',
-    'TopicName': 'test-topic',
-    'DisplayName': 'Test Topic',
-    'Owner': '123456789012',
-    'SubscriptionsPending': 0,
-    'SubscriptionsConfirmed': 1,
-    'SubscriptionsDeleted': 0,
-    'DeliveryPolicy': '',
-    'EffectiveDeliveryPolicy': '',
-    'KmsMasterKeyId': '',
-    'Region': 'us-east-1',
-}]
 
 @patch.object(
-    cartography.intel.aws.sns,
-    "transform_sns_topics",
-    return_value=TEST_TOPIC_DATA
+    cartography.intel.aws.sns, "get_sns_topics", return_value=LIST_TOPICS["Topics"]
 )
-def test_sync_sns(mock_transform, neo4j_session):
+@patch.object(
+    cartography.intel.aws.sns, "get_topic_attributes", return_value=GET_TOPIC_ATTRIBUTES
+)
+def test_sync_sns(mock_get_attributes, mock_get_topics, neo4j_session):
     """
     Test that SNS topics are correctly synced to the graph.
     """
-    
     boto3_session = MagicMock()
     create_test_account(neo4j_session, TEST_ACCOUNT_ID, TEST_UPDATE_TAG)
 
-    
     sync(
         neo4j_session,
         boto3_session,
@@ -48,17 +36,16 @@ def test_sync_sns(mock_transform, neo4j_session):
         {"UPDATE_TAG": TEST_UPDATE_TAG, "AWS_ID": TEST_ACCOUNT_ID},
     )
 
-    
-    assert check_nodes(neo4j_session, "SNSTopic", ["arn"]) == { 
+    assert check_nodes(neo4j_session, "SNSTopic", ["arn"]) == {
         ("arn:aws:sns:us-east-1:123456789012:test-topic",),
     }
-    
+
     assert check_rels(
         neo4j_session,
         "AWSAccount",
         "id",
-        "SNSTopic",  
-        "arn", 
+        "SNSTopic",
+        "arn",
         "RESOURCE",
         rel_direction_right=True,
     ) == {
