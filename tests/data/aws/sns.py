2cd7bee8231220e0329f0bdea3a13082525c0883
AdityaPandeyCN <adityapand3y666@gmail.com>
2025-05-16 12:27:17 +0530

    added mock data for tests
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

---
+++ tests/data/aws/sns.py
@@ -0,0 +1,19 @@
+LIST_TOPICS = {
+    "Topics": [
+        {"TopicArn": "arn:aws:sns:us-east-1:123456789012:test-topic"}
+    ]
+}
+
+GET_TOPIC_ATTRIBUTES = {
+    "Attributes": {
+        "TopicArn": "arn:aws:sns:us-east-1:123456789012:test-topic",
+        "Owner": "123456789012",
+        "DisplayName": "Test Topic",
+        "SubscriptionsPending": "0",
+        "SubscriptionsConfirmed": "1",
+        "SubscriptionsDeleted": "0",
+        "DeliveryPolicy": "{}",
+        "EffectiveDeliveryPolicy": "{}",
+        "KmsMasterKeyId": "arn:aws:kms:us-east-1:123456789012:key/test-key",
+    }
+}
