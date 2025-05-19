80374693dc635ff30b3f66b12c2b297cd17c67d5
Alex Chantavy <alex@subimage.io>
2025-05-18 23:01:02 -0700

    add trivy scanner tests
    Signed-off-by: Alex Chantavy <alex@subimage.io>

--- cartography/client/aws/ecr.py
+++ cartography/client/aws/ecr.py
@@ -21,18 +21,30 @@ def get_ecr_images(
     """
     # See https://community.neo4j.com/t/extract-list-of-nodes-and-labels-from-path/13665/4
     query = """
-    MATCH (e1:ECRRepositoryImage)<-[:REPO_IMAGE]-(repo:ECRRepository)
-    MATCH (repo)<-[:RESOURCE]-(:AWSAccount{id:$AWS_ID})
-    MATCH path = (e1)-[:PARENT*..]->(e2:ECRRepositoryImage) // TODO are there generic OSS ways to infer parent rels
-    WITH reduce(output=[], n in nodes(path) | output + n) as repo_img_collection
-    UNWIND repo_img_collection as repo_img
-        MATCH (er:ECRRepository)-[:REPO_IMAGE]->(repo_img:ECRRepositoryImage)-[:IMAGE]->(img:ECRImage)
-    RETURN DISTINCT
-        er.region as region,
-        repo_img.tag as tag,
-        repo_img.id as uri,
-        er.name as repo_name,
-        img.digest as digest
+MATCH (e1:ECRRepositoryImage)<-[:REPO_IMAGE]-(repo:ECRRepository)
+MATCH (repo)<-[:RESOURCE]-(:AWSAccount {id: $AWS_ID})
+
+// OPTIONAL traversal of parent hierarchy
+OPTIONAL MATCH path = (e1)-[:PARENT*1..]->(ancestor:ECRRepositoryImage)
+WITH e1,
+     CASE
+         WHEN path IS NULL THEN [e1]
+         ELSE [n IN nodes(path) | n] + [e1]
+     END AS repo_img_collection_unflattened
+
+// Flatten and dedupe
+UNWIND repo_img_collection_unflattened AS repo_img
+WITH DISTINCT repo_img
+
+// Match image metadata
+MATCH (er:ECRRepository)-[:REPO_IMAGE]->(repo_img)-[:IMAGE]->(img:ECRImage)
+
+RETURN DISTINCT
+    er.region AS region,
+    repo_img.tag AS tag,
+    repo_img.id AS uri,
+    er.name AS repo_name,
+    img.digest AS digest
     """
     return neo4j_session.read_transaction(
         read_list_of_tuples_tx, query, AWS_ID=aws_account_id
