373fe0652f1795e7ed2bb1c0d5f126180c2a72d5
AdityaPandeyCN <adityapand3y666@gmail.com>
2025-05-23 00:16:45 +0530

    removed linting errors
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- tests/data/aws/secretsmanager.py
+++ tests/data/aws/secretsmanager.py
@@ -10,18 +10,10 @@ LIST_SECRETS = [
         "RotationRules": {"AutomaticallyAfterDays": 90},
         "RotationLambdaARN": "arn:aws:lambda:us-east-1:000000000000:function:test-secret-rotate",
         "KmsKeyId": "arn:aws:kms:us-east-1:000000000000:key/00000000-0000-0000-0000-000000000000",
-        "CreatedDate": datetime.datetime(
-            2024, 12, 15, 10, 30, 0, tzinfo=tz.utc
-        ), 
-        "LastRotatedDate": datetime.datetime(
-            2025, 3, 15, 10, 30, 0, tzinfo=tz.utc
-        ), 
-        "LastChangedDate": datetime.datetime(
-            2025, 4, 20, 14, 45, 30, tzinfo=tz.utc
-        ),  
-        "LastAccessedDate": datetime.datetime(
-            2025, 5, 20, 16, 20, 15, tzinfo=tz.utc
-        ), 
+        "CreatedDate": datetime.datetime(2024, 12, 15, 10, 30, 0, tzinfo=tz.utc),
+        "LastRotatedDate": datetime.datetime(2025, 3, 15, 10, 30, 0, tzinfo=tz.utc),
+        "LastChangedDate": datetime.datetime(2025, 4, 20, 14, 45, 30, tzinfo=tz.utc),
+        "LastAccessedDate": datetime.datetime(2025, 5, 20, 16, 20, 15, tzinfo=tz.utc),
         "PrimaryRegion": "us-west-1",
     },
     {
@@ -41,9 +33,7 @@ LIST_SECRET_VERSIONS = [
         "SecretId": "arn:aws:secretsmanager:us-east-1:000000000000:secret:test-secret-1-000000",
         "VersionId": "00000000-0000-0000-0000-000000000000",
         "VersionStages": ["AWSCURRENT"],
-        "CreatedDate": datetime.datetime(
-            2025, 4, 20, 14, 45, 30, tzinfo=tz.utc
-        ), 
+        "CreatedDate": datetime.datetime(2025, 4, 20, 14, 45, 30, tzinfo=tz.utc),
         "KmsKeyId": "arn:aws:kms:us-east-1:000000000000:key/00000000-0000-0000-0000-000000000000",
     },
     {
@@ -51,9 +41,7 @@ LIST_SECRET_VERSIONS = [
         "SecretId": "arn:aws:secretsmanager:us-east-1:000000000000:secret:test-secret-1-000000",
         "VersionId": "11111111-1111-1111-1111-111111111111",
         "VersionStages": ["AWSPREVIOUS"],
-        "CreatedDate": datetime.datetime(
-            2025, 3, 15, 10, 30, 0, tzinfo=tz.utc
-        ), 
+        "CreatedDate": datetime.datetime(2025, 3, 15, 10, 30, 0, tzinfo=tz.utc),
         "KmsKeyId": "arn:aws:kms:us-east-1:000000000000:key/00000000-0000-0000-0000-000000000000",
     },
 ]
