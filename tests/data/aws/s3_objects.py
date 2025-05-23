5adc9ab62edad73466da9b74ef78cb64e2c4352e
AdityaPandeyCN <adityapand3y666@gmail.com>
2025-05-23 16:48:52 +0530

    minor changes
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- tests/data/aws/s3_objects.py
+++ tests/data/aws/s3_objects.py
@@ -1,67 +1,64 @@
 import datetime
 from datetime import timezone
+from typing import Dict
+from typing import List
 
 LIST_S3_OBJECTS = [
     {
-        'Key': 'documents/report.pdf',
-        'LastModified': datetime.datetime(2025, 5, 20, 10, 0, 0, tzinfo=timezone.utc),
-        'ETag': 'abc123',
-        'Size': 1024000,
-        'StorageClass': 'STANDARD',
+        "Key": "documents/report.pdf",
+        "LastModified": datetime.datetime(2025, 5, 20, 10, 0, 0, tzinfo=timezone.utc),
+        "ETag": "abc123",
+        "Size": 1024000,
+        "StorageClass": "STANDARD",
     },
     {
-        'Key': 'images/logo.png',
-        'LastModified': datetime.datetime(2025, 5, 21, 14, 30, 0, tzinfo=timezone.utc),
-        'ETag': 'def456',
-        'Size': 50000,
-        'StorageClass': 'STANDARD_IA',
-        'Owner': {
-            'DisplayName': 'test-owner',
-            'ID': 'owner-id-123'
-        }
+        "Key": "images/logo.png",
+        "LastModified": datetime.datetime(2025, 5, 21, 14, 30, 0, tzinfo=timezone.utc),
+        "ETag": "def456",
+        "Size": 50000,
+        "StorageClass": "STANDARD_IA",
+        "Owner": {"DisplayName": "test-owner", "ID": "owner-id-123"},
     },
     {
-        'Key': 'archive/old-data.zip',
-        'LastModified': datetime.datetime(2025, 5, 15, 8, 0, 0, tzinfo=timezone.utc),
-        'ETag': 'ghi789',
-        'Size': 5000000,
-        'StorageClass': 'GLACIER',
-        'RestoreStatus': {
-            'IsRestoreInProgress': True,
-            'RestoreExpiryDate': datetime.datetime(2025, 6, 1, 0, 0, 0, tzinfo=timezone.utc)
-        }
-    }
+        "Key": "archive/old-data.zip",
+        "LastModified": datetime.datetime(2025, 5, 15, 8, 0, 0, tzinfo=timezone.utc),
+        "ETag": "ghi789",
+        "Size": 5000000,
+        "StorageClass": "GLACIER",
+        "RestoreStatus": {
+            "IsRestoreInProgress": True,
+            "RestoreExpiryDate": datetime.datetime(
+                2025, 6, 1, 0, 0, 0, tzinfo=timezone.utc
+            ),
+        },
+    },
 ]
 
-# Empty bucket response
-EMPTY_BUCKET_OBJECTS = []
+EMPTY_BUCKET_OBJECTS: List[Dict] = []
 
-# Single object with owner info
 SINGLE_OBJECT_WITH_OWNER = [
     {
-        'Key': 'images/logo.png',
-        'LastModified': datetime.datetime(2025, 5, 21, 14, 30, 0, tzinfo=timezone.utc),
-        'ETag': 'def456',
-        'Size': 50000,
-        'StorageClass': 'STANDARD_IA',
-        'Owner': {
-            'DisplayName': 'test-owner',
-            'ID': 'owner-id-123'
-        }
+        "Key": "images/logo.png",
+        "LastModified": datetime.datetime(2025, 5, 21, 14, 30, 0, tzinfo=timezone.utc),
+        "ETag": "def456",
+        "Size": 50000,
+        "StorageClass": "STANDARD_IA",
+        "Owner": {"DisplayName": "test-owner", "ID": "owner-id-123"},
     }
 ]
 
-# Single Glacier object with restore status
 SINGLE_GLACIER_OBJECT = [
     {
-        'Key': 'archive/old-data.zip',
-        'LastModified': datetime.datetime(2025, 5, 15, 8, 0, 0, tzinfo=timezone.utc),
-        'ETag': 'ghi789',
-        'Size': 5000000,
-        'StorageClass': 'GLACIER',
-        'RestoreStatus': {
-            'IsRestoreInProgress': True,
-            'RestoreExpiryDate': datetime.datetime(2025, 6, 1, 0, 0, 0, tzinfo=timezone.utc)
-        }
+        "Key": "archive/old-data.zip",
+        "LastModified": datetime.datetime(2025, 5, 15, 8, 0, 0, tzinfo=timezone.utc),
+        "ETag": "ghi789",
+        "Size": 5000000,
+        "StorageClass": "GLACIER",
+        "RestoreStatus": {
+            "IsRestoreInProgress": True,
+            "RestoreExpiryDate": datetime.datetime(
+                2025, 6, 1, 0, 0, 0, tzinfo=timezone.utc
+            ),
+        },
     }
-] 
+]
