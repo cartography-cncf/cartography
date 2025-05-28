b175d96c684847f4dc73e9cd6c9e79b928008686
Jeremy Chapeau <jeremy.chapeau@gmail.com>
2025-05-28 16:06:31 +0200

    add tests data
    Signed-off-by: Jeremy Chapeau <jeremy.chapeau@gmail.com>

---
+++ tests/data/airbyte/users.py
@@ -0,0 +1,22 @@
+AIRBYTE_USERS = [
+    {
+        "id": "eae5cd19-72c4-49b0-87b3-e2f0c99344a3",
+        "name": "Marge Simpson",
+        "email": "mbsimpson@simpson.corp",
+    },
+    {
+        "id": "9507b572-7f70-4eba-94fe-baf54fdc05ae",
+        "name": "Homer Simpson",
+        "email": "hjsimpson@simpson.corp",
+    },
+]
+
+AIRBYTE_USERS_PERMISSIONS = [
+    {
+        "permissionId": "d6d45c74-75d5-4a68-b591-cc175361aa79",
+        "permissionType": "organization_admin",
+        "userId": '"eae5cd19-72c4-49b0-87b3-e2f0c99344a3',
+        "scopeId": "31634962-4b3c-4b0c-810d-a2a77d6df222",
+        "scope": "organization",
+    }
+]
