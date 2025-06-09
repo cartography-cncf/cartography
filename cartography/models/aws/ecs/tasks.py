08e5ee68eb57d7f4d2e66d2a911825717eb3aefb
Alex Chantavy <alex@subimage.io>
2025-06-09 15:40:51 -0700

    Refactor ECS module to new data model

---
+++ cartography/models/aws/ecs/tasks.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+
+from cartography.models.core.common import PropertyRef
+from cartography.models.core.nodes import CartographyNodeProperties, CartographyNodeSchema
+from cartography.models.core.relationships import (
+    CartographyRelProperties,
+    CartographyRelSchema,
+    LinkDirection,
+    make_target_node_matcher,
+    TargetNodeMatcher,
+    OtherRelationships,
+)
+
+
+@dataclass(frozen=True)
+class ECSTaskNodeProperties(CartographyNodeProperties):
+    id: PropertyRef = PropertyRef("taskArn")
+    arn: PropertyRef = PropertyRef("taskArn", extra_index=True)
+    availability_zone: PropertyRef = PropertyRef("availabilityZone")
+    capacity_provider_name: PropertyRef = PropertyRef("capacityProviderName")
+    cluster_arn: PropertyRef = PropertyRef("clusterArn")
+    connectivity: PropertyRef = PropertyRef("connectivity")
+    connectivity_at: PropertyRef = PropertyRef("connectivityAt")
+    container_instance_arn: PropertyRef = PropertyRef("containerInstanceArn")
+    cpu: PropertyRef = PropertyRef("cpu")
+    created_at: PropertyRef = PropertyRef("createdAt")
+    desired_status: PropertyRef = PropertyRef("desiredStatus")
+    enable_execute_command: PropertyRef = PropertyRef("enableExecuteCommand")
+    execution_stopped_at: PropertyRef = PropertyRef("executionStoppedAt")
+    group: PropertyRef = PropertyRef("group")
+    health_status: PropertyRef = PropertyRef("healthStatus")
+    last_status: PropertyRef = PropertyRef("lastStatus")
+    launch_type: PropertyRef = PropertyRef("launchType")
+    memory: PropertyRef = PropertyRef("memory")
+    platform_version: PropertyRef = PropertyRef("platformVersion")
+    platform_family: PropertyRef = PropertyRef("platformFamily")
+    pull_started_at: PropertyRef = PropertyRef("pullStartedAt")
+    pull_stopped_at: PropertyRef = PropertyRef("pullStoppedAt")
+    started_at: PropertyRef = PropertyRef("startedAt")
+    started_by: PropertyRef = PropertyRef("startedBy")
+    stop_code: PropertyRef = PropertyRef("stopCode")
+    stopped_at: PropertyRef = PropertyRef("stoppedAt")
+    stopped_reason: PropertyRef = PropertyRef("stoppedReason")
+    stopping_at: PropertyRef = PropertyRef("stoppingAt")
+    task_definition_arn: PropertyRef = PropertyRef("taskDefinitionArn")
+    version: PropertyRef = PropertyRef("version")
+    ephemeral_storage_size_in_gib: PropertyRef = PropertyRef("ephemeralStorage.sizeInGiB")
+    region: PropertyRef = PropertyRef("Region", set_in_kwargs=True)
+    lastupdated: PropertyRef = PropertyRef("lastupdated", set_in_kwargs=True)
+
+
+@dataclass(frozen=True)
+class ECSTaskToECSClusterRelProperties(CartographyRelProperties):
+    lastupdated: PropertyRef = PropertyRef("lastupdated", set_in_kwargs=True)
+
+
+@dataclass(frozen=True)
+class ECSTaskToECSClusterRel(CartographyRelSchema):
+    target_node_label: str = "ECSCluster"
+    target_node_matcher: TargetNodeMatcher = make_target_node_matcher({"id": PropertyRef("ClusterArn", set_in_kwargs=True)})
+    direction: LinkDirection = LinkDirection.INWARD
+    rel_label: str = "HAS_TASK"
+    properties: ECSTaskToECSClusterRelProperties = ECSTaskToECSClusterRelProperties()
+
+
+@dataclass(frozen=True)
+class ECSTaskToTaskDefinitionRelProperties(CartographyRelProperties):
+    lastupdated: PropertyRef = PropertyRef("lastupdated", set_in_kwargs=True)
+
+
+@dataclass(frozen=True)
+class ECSTaskToTaskDefinitionRel(CartographyRelSchema):
+    target_node_label: str = "ECSTaskDefinition"
+    target_node_matcher: TargetNodeMatcher = make_target_node_matcher({"id": PropertyRef("taskDefinitionArn")})
+    direction: LinkDirection = LinkDirection.OUTWARD
+    rel_label: str = "HAS_TASK_DEFINITION"
+    properties: ECSTaskToTaskDefinitionRelProperties = ECSTaskToTaskDefinitionRelProperties()
+
+
+@dataclass(frozen=True)
+class ECSTaskToContainerInstanceRelProperties(CartographyRelProperties):
+    lastupdated: PropertyRef = PropertyRef("lastupdated", set_in_kwargs=True)
+
+
+@dataclass(frozen=True)
+class ECSTaskToContainerInstanceRel(CartographyRelSchema):
+    target_node_label: str = "ECSContainerInstance"
+    target_node_matcher: TargetNodeMatcher = make_target_node_matcher({"id": PropertyRef("containerInstanceArn")})
+    direction: LinkDirection = LinkDirection.INWARD
+    rel_label: str = "HAS_TASK"
+    properties: ECSTaskToContainerInstanceRelProperties = ECSTaskToContainerInstanceRelProperties()
+
+
+@dataclass(frozen=True)
+class ECSTaskSchema(CartographyNodeSchema):
+    label: str = "ECSTask"
+    properties: ECSTaskNodeProperties = ECSTaskNodeProperties()
+    sub_resource_relationship: ECSTaskToECSClusterRel = ECSTaskToECSClusterRel()
+    other_relationships: OtherRelationships = OtherRelationships([
+        ECSTaskToTaskDefinitionRel(),
+        ECSTaskToContainerInstanceRel(),
+    ])
+
