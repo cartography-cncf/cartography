3e6d5f762a263f3bcf817ad9391653c959851c15
Sergio Franco <sergiof@lyft.com>
2025-07-15 12:34:32 -0600

    Implement application ingestion for SentinelOne intel module
    ### Summary
    This commit introduces the ingestion of application data for the SentinelOne intel module, including:
    
    - New data models for applications and application versions.
    - Sync job to retrieve and load application data into Neo4j.
    - Utility functions for handling application data and versioning.
    - Updates to existing ingestion logic to accommodate application syncing.
    
    ### Related issues or links
    - https://github.com/cartography-cncf/cartography/issues/1660
    
    ### Checklist
    - [x] Update/add unit or integration tests.
    - [x] Include console log trace showing what happened before and after your changes.
    - [x] Update the schema documentation.
    
    ---------
    Signed-off by Sergio Franco <sergiof@lyft.com>
    
    Signed-off-by: Sergio Franco <sergiof@lyft.com>

--- cartography/intel/sentinelone/utils.py
+++ cartography/intel/sentinelone/utils.py
@@ -7,3 +7,17 @@ def get_application_id(name: str, vendor: str) -> str:
     name_normalized = re.sub(r"[^\w]", "", name_normalized)
     vendor_normalized = re.sub(r"[^\w\s]", "", vendor_normalized)
     return f"{vendor_normalized}:{name_normalized}"
+
+
+def get_application_version_id(name: str, vendor: str, version: str) -> str:
+    """
+    Generate a unique identifier for an application version
+    :param name: Application name
+    :param vendor: Application vendor
+    :param version: Application version
+    :return: Unique identifier for the application version
+    """
+    app_id = get_application_id(name, vendor)
+    version_normalized = version.strip().lower().replace(" ", "_")
+    version_normalized = re.sub(r"[^\w.-]", "", version_normalized)
+    return f"{app_id}:{version_normalized}"
