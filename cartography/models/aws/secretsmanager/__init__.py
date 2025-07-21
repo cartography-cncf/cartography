f0ceb82c28e2842904c51b5a5ef0cdd35072ebac
Shyam Mukund <shyammukund@Shyams-MacBook-Pro.local>
2025-07-21 15:40:49 -0700

    refactor AWS Secrets Manager module to use data model
    Signed-off-by: Shyam Mukund <shyammukund@Shyams-MacBook-Pro.local>

---
+++ cartography/models/aws/secretsmanager/__init__.py
@@ -0,0 +1,9 @@
+from cartography.models.aws.secretsmanager.secret import SecretsManagerSecretSchema
+from cartography.models.aws.secretsmanager.secret_version import (
+    SecretsManagerSecretVersionSchema,
+)
+
+__all__ = [
+    "SecretsManagerSecretSchema",
+    "SecretsManagerSecretVersionSchema",
+]
