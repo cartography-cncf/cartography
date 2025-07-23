6547a461e5aa65a3fcb089ca5d7a8c7ee350229c
shyammukund <shyam@subimage.io>
2025-07-22 17:26:23 -0700

    removed unecessary stuff in init.py
    Signed-off-by: shyammukund <shyam@subimage.io>

--- cartography/models/aws/secretsmanager/__init__.py
+++ cartography/models/aws/secretsmanager/__init__.py
@@ -1,9 +1,1 @@
-from cartography.models.aws.secretsmanager.secret import SecretsManagerSecretSchema
-from cartography.models.aws.secretsmanager.secret_version import (
-    SecretsManagerSecretVersionSchema,
-)
 
-__all__ = [
-    "SecretsManagerSecretSchema",
-    "SecretsManagerSecretVersionSchema",
-]
