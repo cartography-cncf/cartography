104af7d07cc1688a7bb4d0c8416fc6101f2db91f
shyammukund <shyam@subimage.io>
2025-07-28 16:07:14 -0700

    AWS Lambda Module Refactor
    Signed-off-by: shyammukund <shyam@subimage.io>

---
+++ cartography/models/aws/lambda_function/layer.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+
+from cartography.models.core.common import PropertyRef
+from cartography.models.core.nodes import CartographyNodeProperties
+from cartography.models.core.nodes import CartographyNodeSchema
+from cartography.models.core.relationships import CartographyRelProperties
+from cartography.models.core.relationships import CartographyRelSchema
+from cartography.models.core.relationships import LinkDirection
+from cartography.models.core.relationships import make_target_node_matcher
+from cartography.models.core.relationships import make_source_node_matcher
+from cartography.models.core.relationships import TargetNodeMatcher
+from cartography.models.core.relationships import SourceNodeMatcher
+
+
+@dataclass(frozen=True)
+class AWSLambdaLayerNodeProperties(CartographyNodeProperties):
+    id: PropertyRef = PropertyRef("Arn")
+    arn: PropertyRef = PropertyRef("Arn")
+    codesize: PropertyRef = PropertyRef("CodeSize")
+    signingprofileversionarn: PropertyRef = PropertyRef("SigningProfileVersionArn")
+    signingjobarn: PropertyRef = PropertyRef("SigningJobArn")
+    lastupdated: PropertyRef = PropertyRef("lastupdated", set_in_kwargs=True)
+
+
+# Sub-resource relationship: (:AWSAccount)-[:RESOURCE]->(:AWSLambdaLayer)
+@dataclass(frozen=True)
+class AWSLambdaLayerToAWSAccountRelProperties(CartographyRelProperties):
+    lastupdated: PropertyRef = PropertyRef("lastupdated", set_in_kwargs=True)
+
+
+@dataclass(frozen=True)
+class AWSLambdaLayerToAWSAccountRel(CartographyRelSchema):
+    target_node_label: str = "AWSAccount"
+    target_node_matcher: TargetNodeMatcher = make_target_node_matcher(
+        {"id": PropertyRef("AWS_ID", set_in_kwargs=True)},
+    )
+    direction: LinkDirection = LinkDirection.INWARD
+    rel_label: str = "RESOURCE"
+    properties: AWSLambdaLayerToAWSAccountRelProperties = AWSLambdaLayerToAWSAccountRelProperties()
+
+
+# Matchlink relationship: (:AWSLambda)-[:HAS]->(:AWSLambdaLayer)
+@dataclass(frozen=True)
+class AWSLambdaToLayerRelProperties(CartographyRelProperties):
+    lastupdated: PropertyRef = PropertyRef("lastupdated", set_in_kwargs=True)
+    _sub_resource_label: PropertyRef = PropertyRef("_sub_resource_label", set_in_kwargs=True)
+    _sub_resource_id: PropertyRef = PropertyRef("_sub_resource_id", set_in_kwargs=True)
+
+
+@dataclass(frozen=True)
+class AWSLambdaToLayerRel(CartographyRelSchema):
+    target_node_label: str = "AWSLambdaLayer"
+    target_node_matcher: TargetNodeMatcher = make_target_node_matcher(
+        {"id": PropertyRef("Arn")},
+    )
+    direction: LinkDirection = LinkDirection.OUTWARD
+    rel_label: str = "HAS"
+    properties: AWSLambdaToLayerRelProperties = AWSLambdaToLayerRelProperties()
+    source_node_label: str = "AWSLambda"
+    source_node_matcher: SourceNodeMatcher = make_source_node_matcher(
+        {"id": PropertyRef("FunctionArn")},
+    )
+
+
+@dataclass(frozen=True)
+class AWSLambdaLayerSchema(CartographyNodeSchema):
+    label: str = "AWSLambdaLayer"
+    properties: AWSLambdaLayerNodeProperties = AWSLambdaLayerNodeProperties()
+    sub_resource_relationship: AWSLambdaLayerToAWSAccountRel = AWSLambdaLayerToAWSAccountRel() 
--- tests/integration/cartography/intel/aws/test_lambda.py
+++ tests/integration/cartography/intel/aws/test_lambda.py
@@ -1,5 +1,7 @@
 import cartography.intel.aws.lambda_function
 import tests.data.aws.lambda_function
+from tests.integration.util import check_nodes
+from tests.integration.util import check_rels
 
 TEST_ACCOUNT_ID = "000000000000"
 TEST_REGION = "us-west-2"
@@ -8,37 +10,34 @@ TEST_UPDATE_TAG = 123456789
 
 def test_load_lambda_functions(neo4j_session):
     data = tests.data.aws.lambda_function.LIST_LAMBDA_FUNCTIONS
+    
+    # Transform the data first
+    transformed_data = cartography.intel.aws.lambda_function.transform_lambda_functions(
+        data,
+        TEST_REGION,
+    )
 
     cartography.intel.aws.lambda_function.load_lambda_functions(
         neo4j_session,
-        data,
+        transformed_data,
         TEST_REGION,
         TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
     )
 
-    expected_nodes = {
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-1",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-2",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-4",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-5",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-6",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-7",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-8",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-9",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-10",
+    # Test Lambda nodes were created correctly
+    assert check_nodes(neo4j_session, "AWSLambda", ["id"]) == {
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-1",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-2",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-3",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-4",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-5",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-6",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-7",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-8",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-9",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-10",),
     }
-
-    nodes = neo4j_session.run(
-        """
-        MATCH (r:AWSLambda) RETURN r.id;
-        """,
-    )
-
-    actual_nodes = {n["r.id"] for n in nodes}
-
-    assert actual_nodes == expected_nodes
 
 
 def test_load_lambda_relationships(neo4j_session):
@@ -55,8 +54,48 @@ def test_load_lambda_relationships(neo4j_session):
 
     # Load Test Lambda Functions
     data = tests.data.aws.lambda_function.LIST_LAMBDA_FUNCTIONS
+    
+    # Transform the data first
+    transformed_data = cartography.intel.aws.lambda_function.transform_lambda_functions(
+        data,
+        TEST_REGION,
+    )
 
     cartography.intel.aws.lambda_function.load_lambda_functions(
+        neo4j_session,
+        transformed_data,
+        TEST_REGION,
+        TEST_ACCOUNT_ID,
+        TEST_UPDATE_TAG,
+    )
+
+    # Test AWSAccount -> AWSLambda RESOURCE relationships
+    assert check_rels(
+        neo4j_session,
+        "AWSAccount",
+        "id",
+        "AWSLambda",
+        "id",
+        "RESOURCE",
+        rel_direction_right=True,
+    ) == {
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-1"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-2"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-4"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-5"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-6"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-7"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-8"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-9"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-10"),
+    }
+
+
+def test_load_lambda_function_aliases(neo4j_session):
+    data = tests.data.aws.lambda_function.LIST_LAMBDA_FUNCTION_ALIASES
+
+    cartography.intel.aws.lambda_function.load_lambda_function_aliases(
         neo4j_session,
         data,
         TEST_REGION,
@@ -64,93 +103,42 @@ def test_load_lambda_relationships(neo4j_session):
         TEST_UPDATE_TAG,
     )
 
-    expected_nodes = {
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-1",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-2",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-4",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-5",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-6",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-7",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-8",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-9",
-        ),
-        (
-            TEST_ACCOUNT_ID,
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-10",
-        ),
+    # Test Lambda alias nodes were created correctly
+    assert check_nodes(neo4j_session, "AWSLambdaFunctionAlias", ["id"]) == {
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-3:LIVE",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-9:LIVE",),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-10:LIVE",),
     }
 
-    # Fetch relationships
-    result = neo4j_session.run(
-        """
-        MATCH (n1:AWSAccount)-[:RESOURCE]->(n2:AWSLambda) RETURN n1.id, n2.id;
-        """,
-    )
-    actual = {(r["n1.id"], r["n2.id"]) for r in result}
-
-    assert actual == expected_nodes
-
-
-def test_load_lambda_function_aliases(neo4j_session):
-    data = tests.data.aws.lambda_function.LIST_LAMBDA_FUNCTION_ALIASES
-
-    cartography.intel.aws.lambda_function._load_lambda_function_aliases(
+    # Test AWSAccount -> AWSLambdaFunctionAlias RESOURCE relationships
+    assert check_rels(
         neo4j_session,
-        data,
-        TEST_UPDATE_TAG,
-    )
-
-    expected_nodes = {
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3:LIVE",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-9:LIVE",
-        "arn:aws:lambda:us-west-2:000000000000:function:sample-function-10:LIVE",
+        "AWSAccount",
+        "id",
+        "AWSLambdaFunctionAlias",
+        "id",
+        "RESOURCE",
+        rel_direction_right=True,
+    ) == {
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3:LIVE"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-9:LIVE"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-west-2:000000000000:function:sample-function-10:LIVE"),
     }
-
-    nodes = neo4j_session.run(
-        """
-        MATCH (r:AWSLambdaFunctionAlias) RETURN r.id;
-        """,
-    )
-
-    actual_nodes = {n["r.id"] for n in nodes}
-
-    assert actual_nodes == expected_nodes
 
 
 def test_load_lambda_function_aliases_relationships(neo4j_session):
     # Create Test Lambda Function
     data = tests.data.aws.lambda_function.LIST_LAMBDA_FUNCTIONS
+    
+    # Transform the data first
+    transformed_data = cartography.intel.aws.lambda_function.transform_lambda_functions(
+        data,
+        TEST_REGION,
+    )
 
     cartography.intel.aws.lambda_function.load_lambda_functions(
         neo4j_session,
-        data,
+        transformed_data,
         TEST_REGION,
         TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
@@ -158,70 +146,74 @@ def test_load_lambda_function_aliases_relationships(neo4j_session):
 
     aliases = tests.data.aws.lambda_function.LIST_LAMBDA_FUNCTION_ALIASES
 
-    cartography.intel.aws.lambda_function._load_lambda_function_aliases(
+    cartography.intel.aws.lambda_function.load_lambda_function_aliases(
         neo4j_session,
         aliases,
+        TEST_REGION,
+        TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
     )
 
-    expected_nodes = {
-        (
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3",
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3:LIVE",
-        ),
-        (
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-9",
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-9:LIVE",
-        ),
-        (
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-10",
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-10:LIVE",
-        ),
+    # Test AWSLambda -> AWSLambdaFunctionAlias KNOWN_AS relationships
+    assert check_rels(
+        neo4j_session,
+        "AWSLambda",
+        "id",
+        "AWSLambdaFunctionAlias",
+        "id",
+        "KNOWN_AS",
+        rel_direction_right=True,
+    ) == {
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-3", "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3:LIVE"),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-9", "arn:aws:lambda:us-west-2:000000000000:function:sample-function-9:LIVE"),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-10", "arn:aws:lambda:us-west-2:000000000000:function:sample-function-10:LIVE"),
     }
-
-    # Fetch relationships
-    result = neo4j_session.run(
-        """
-        MATCH (n1:AWSLambda)-[:KNOWN_AS]->(n2:AWSLambdaFunctionAlias) RETURN n1.id, n2.id;
-        """,
-    )
-    actual = {(r["n1.id"], r["n2.id"]) for r in result}
-
-    assert actual == expected_nodes
 
 
 def test_load_lambda_event_source_mappings(neo4j_session):
     data = tests.data.aws.lambda_function.LIST_EVENT_SOURCE_MAPPINGS
 
-    cartography.intel.aws.lambda_function._load_lambda_event_source_mappings(
+    cartography.intel.aws.lambda_function.load_lambda_event_source_mappings(
         neo4j_session,
         data,
+        TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
     )
 
-    expected_nodes = {
-        "i01",
-        "i02",
+    # Test Lambda event source mapping nodes were created correctly
+    assert check_nodes(neo4j_session, "AWSLambdaEventSourceMapping", ["id"]) == {
+        ("i01",),
+        ("i02",),
     }
 
-    nodes = neo4j_session.run(
-        """
-        MATCH (r:AWSLambdaEventSourceMapping) RETURN r.id;
-        """,
-    )
-
-    actual_nodes = {n["r.id"] for n in nodes}
-
-    assert actual_nodes == expected_nodes
+    # Test AWSAccount -> AWSLambdaEventSourceMapping RESOURCE relationships
+    assert check_rels(
+        neo4j_session,
+        "AWSAccount",
+        "id",
+        "AWSLambdaEventSourceMapping",
+        "id",
+        "RESOURCE",
+        rel_direction_right=True,
+    ) == {
+        (TEST_ACCOUNT_ID, "i01"),
+        (TEST_ACCOUNT_ID, "i02"),
+    }
 
 
 def test_load_lambda_event_source_mappings_relationships(neo4j_session):
     # Create Test Lambda Function
     data = tests.data.aws.lambda_function.LIST_LAMBDA_FUNCTIONS
+    
+    # Transform the data first
+    transformed_data = cartography.intel.aws.lambda_function.transform_lambda_functions(
+        data,
+        TEST_REGION,
+    )
 
     cartography.intel.aws.lambda_function.load_lambda_functions(
         neo4j_session,
-        data,
+        transformed_data,
         TEST_REGION,
         TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
@@ -229,67 +221,74 @@ def test_load_lambda_event_source_mappings_relationships(neo4j_session):
 
     esm = tests.data.aws.lambda_function.LIST_EVENT_SOURCE_MAPPINGS
 
-    cartography.intel.aws.lambda_function._load_lambda_event_source_mappings(
+    cartography.intel.aws.lambda_function.load_lambda_event_source_mappings(
         neo4j_session,
         esm,
+        TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
     )
 
-    expected_nodes = {
-        (
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-7",
-            "i01",
-        ),
-        (
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-8",
-            "i02",
-        ),
+    # Test AWSLambda -> AWSLambdaEventSourceMapping RESOURCE relationships
+    assert check_rels(
+        neo4j_session,
+        "AWSLambda",
+        "id",
+        "AWSLambdaEventSourceMapping",
+        "id",
+        "RESOURCE",
+        rel_direction_right=True,
+    ) == {
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-7", "i01"),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-8", "i02"),
     }
-
-    # Fetch relationships
-    result = neo4j_session.run(
-        """
-        MATCH (n1:AWSLambda)-[:RESOURCE]->(n2:AWSLambdaEventSourceMapping) RETURN n1.id, n2.id;
-        """,
-    )
-    actual = {(r["n1.id"], r["n2.id"]) for r in result}
-
-    assert actual == expected_nodes
 
 
 def test_load_lambda_layers(neo4j_session):
     data = tests.data.aws.lambda_function.LIST_LAYERS
 
-    cartography.intel.aws.lambda_function._load_lambda_layers(
+    cartography.intel.aws.lambda_function.load_lambda_layers(
         neo4j_session,
         data,
+        TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
     )
 
-    expected_nodes = {
-        "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-1",
-        "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-2",
-        "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-3",
+    # Test Lambda layer nodes were created correctly
+    assert check_nodes(neo4j_session, "AWSLambdaLayer", ["id"]) == {
+        ("arn:aws:lambda:us-east-2:123456789012:layer:my-layer-1",),
+        ("arn:aws:lambda:us-east-2:123456789012:layer:my-layer-2",),
+        ("arn:aws:lambda:us-east-2:123456789012:layer:my-layer-3",),
     }
 
-    nodes = neo4j_session.run(
-        """
-        MATCH (r:AWSLambdaLayer) RETURN r.id;
-        """,
-    )
-
-    actual_nodes = {n["r.id"] for n in nodes}
-
-    assert actual_nodes == expected_nodes
+    # Test AWSAccount -> AWSLambdaLayer RESOURCE relationships
+    assert check_rels(
+        neo4j_session,
+        "AWSAccount",
+        "id",
+        "AWSLambdaLayer",
+        "id",
+        "RESOURCE",
+        rel_direction_right=True,
+    ) == {
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-1"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-2"),
+        (TEST_ACCOUNT_ID, "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-3"),
+    }
 
 
 def test_load_lambda_layers_relationships(neo4j_session):
     # Create Test Lambda Function
     data = tests.data.aws.lambda_function.LIST_LAMBDA_FUNCTIONS
+    
+    # Transform the data first
+    transformed_data = cartography.intel.aws.lambda_function.transform_lambda_functions(
+        data,
+        TEST_REGION,
+    )
 
     cartography.intel.aws.lambda_function.load_lambda_functions(
         neo4j_session,
-        data,
+        transformed_data,
         TEST_REGION,
         TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
@@ -297,33 +296,24 @@ def test_load_lambda_layers_relationships(neo4j_session):
 
     layers = tests.data.aws.lambda_function.LIST_LAYERS
 
-    cartography.intel.aws.lambda_function._load_lambda_layers(
+    cartography.intel.aws.lambda_function.load_lambda_layers(
         neo4j_session,
         layers,
+        TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
     )
 
-    expected_nodes = {
-        (
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-2",
-            "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-1",
-        ),
-        (
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-3",
-            "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-2",
-        ),
-        (
-            "arn:aws:lambda:us-west-2:000000000000:function:sample-function-4",
-            "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-3",
-        ),
+    # Test AWSLambda -> AWSLambdaLayer HAS relationships
+    assert check_rels(
+        neo4j_session,
+        "AWSLambda",
+        "id",
+        "AWSLambdaLayer",
+        "id",
+        "HAS",
+        rel_direction_right=True,
+    ) == {
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-2", "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-1"),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-3", "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-2"),
+        ("arn:aws:lambda:us-west-2:000000000000:function:sample-function-4", "arn:aws:lambda:us-east-2:123456789012:layer:my-layer-3"),
     }
-
-    # Fetch relationships
-    result = neo4j_session.run(
-        """
-        MATCH (n1:AWSLambda)-[:HAS]->(n2:AWSLambdaLayer) RETURN n1.id, n2.id;
-        """,
-    )
-    actual = {(r["n1.id"], r["n2.id"]) for r in result}
-
-    assert actual == expected_nodes
