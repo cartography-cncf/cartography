befa09e15fbe0298158b0bd765c70cc14fba66bf
shyammukund <shyam@subimage.io>
2025-08-04 16:08:12 -0700

    fix lint
    Signed-off-by: shyammukund <shyam@subimage.io>

--- tests/unit/cartography/intel/aws/test_kms.py
+++ tests/unit/cartography/intel/aws/test_kms.py
@@ -1,5 +1,3 @@
-import pytest
-
 from cartography.intel.aws.kms import transform_kms_key_policies
 from tests.data.aws.kms import ACCESS_DENIED_KMS_KEY_DETAILS
 
@@ -15,15 +13,15 @@ def test_transform_kms_key_policies_with_null_policy():
 
     # Assert - Keys with None policy should have default empty policy data
     assert len(result) == 2
-    
+
     # First key
     key1_data = result["9a1ad414-6e3b-47ce-8366-6b8f26ba467d"]
     assert key1_data["kms_key"] == "9a1ad414-6e3b-47ce-8366-6b8f26ba467d"
     assert key1_data["anonymous_access"] is False
     assert key1_data["anonymous_actions"] == []
-    
+
     # Second key
     key2_data = result["1b2cd345-7e8f-49ab-cdef-0123456789ab"]
     assert key2_data["kms_key"] == "1b2cd345-7e8f-49ab-cdef-0123456789ab"
     assert key2_data["anonymous_access"] is False
-    assert key2_data["anonymous_actions"] == [] 
+    assert key2_data["anonymous_actions"] == []
