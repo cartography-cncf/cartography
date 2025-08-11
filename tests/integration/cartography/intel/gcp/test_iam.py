d7991137eca561eef6b9068e4c4fa7accd522d65
wahidullah <wahidmaqs49@gmail.com>
2025-08-11 18:02:28 +0530

    test: add sync test for loading GCP roles and service accounts
    Signed-off-by: wahidullah <wahidmaqs49@gmail.com>

--- tests/integration/cartography/intel/gcp/test_iam.py
+++ tests/integration/cartography/intel/gcp/test_iam.py
@@ -96,3 +96,59 @@ def test_load_gcp_service_accounts(neo4j_session):
         )
         == expected_rels
     )
+
+def test_sync(neo4j_session):
+    _create_test_project(neo4j_session)
+    roles_data = tests.data.gcp.iam.LIST_ROLES_RESPONSE["roles"]
+    sa_data = tests.data.gcp.iam.LIST_SERVICE_ACCOUNTS_RESPONSE["accounts"]
+
+    cartography.intel.gcp.iam.load_gcp_roles(
+        neo4j_session,
+        roles_data,
+        TEST_PROJECT_ID,
+        TEST_UPDATE_TAG,
+    )
+    cartography.intel.gcp.iam.load_gcp_service_accounts(
+        neo4j_session,
+        sa_data,
+        TEST_PROJECT_ID,
+        TEST_UPDATE_TAG,
+    )
+
+    # Assert Roles
+    expected_role_nodes = {
+        ("projects/project-123/roles/customRole1",),
+        ("roles/editor",),
+        ("projects/project-123/roles/customRole2",),
+    }
+    assert check_nodes(neo4j_session, "GCPRole", ["id"]) == expected_role_nodes
+
+    expected_role_rels = {
+        (TEST_PROJECT_ID, "projects/project-123/roles/customRole1"),
+        (TEST_PROJECT_ID, "roles/editor"),
+        (TEST_PROJECT_ID, "projects/project-123/roles/customRole2"),
+    }
+    assert check_rels(
+        neo4j_session,
+        "GCPProject", "id",
+        "GCPRole", "name",
+        "RESOURCE",
+    ) == expected_role_rels
+
+    # Assert Service Accounts
+    expected_sa_nodes = {
+        ("112233445566778899",),
+        ("998877665544332211",),
+    }
+    assert check_nodes(neo4j_session, "GCPServiceAccount", ["id"]) == expected_sa_nodes
+
+    expected_sa_rels = {
+        (TEST_PROJECT_ID, "112233445566778899"),
+        (TEST_PROJECT_ID, "998877665544332211"),
+    }
+    assert check_rels(
+        neo4j_session,
+        "GCPProject", "id",
+        "GCPServiceAccount", "id",
+        "RESOURCE",
+    ) == expected_sa_rels
