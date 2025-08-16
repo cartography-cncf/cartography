90ea3da28488597d82548f6c9d7a235e871cbd27
wahidullah <wahidmaqs49@gmail.com>
2025-08-16 19:20:32 +0530

    test: add integration test for sync() with GCP IAM using  patch
    Signed-off-by: wahidullah <wahidmaqs49@gmail.com>

--- tests/integration/cartography/intel/gcp/test_iam.py
+++ tests/integration/cartography/intel/gcp/test_iam.py
@@ -2,6 +2,7 @@ import cartography.intel.gcp.iam
 import tests.data.gcp.iam
 from tests.integration.util import check_nodes
 from tests.integration.util import check_rels
+from unittest.mock import patch
 
 TEST_PROJECT_ID = "project-123"
 TEST_UPDATE_TAG = 123456789
@@ -98,85 +99,30 @@ def test_load_gcp_service_accounts(neo4j_session):
     )
 
 def test_sync_gcp_iam(neo4j_session):
-    """Integration test for sync() using static IAM test data."""
-    
     _create_test_project(neo4j_session)
-
-    def sa_execute():
-        return tests.data.gcp.iam.LIST_SERVICE_ACCOUNTS_RESPONSE
-
-    def custom_roles_execute():
-        custom_roles = [
-            role for role in tests.data.gcp.iam.LIST_ROLES_RESPONSE["roles"]
-            if role["name"].startswith("projects/")
-        ]
-        return {"roles": custom_roles}
-
-    def predefined_roles_execute():
-        predefined_roles = [
-            role for role in tests.data.gcp.iam.LIST_ROLES_RESPONSE["roles"]
-            if role["name"].startswith("roles/")
-        ]
-        return {"roles": predefined_roles}
-
-    def list_next(*args, **kwargs):
-        return None
-
-    # List methods that wrap the execute functions
-    def sa_list(name):
-        return type("Request", (), {"execute": sa_execute})()
-
-    def custom_roles_list(parent):
-        return type("Request", (), {"execute": custom_roles_execute})()
-
-    def predefined_roles_list(view):
-        return type("Request", (), {"execute": predefined_roles_execute})()
-
-    # Builders for the API client
-    def service_accounts():
-        return type("ServiceAccounts", (), {
-            "list": sa_list,
-            "list_next": list_next,
-        })()
-
-    def project_roles():
-        return type("ProjectRoles", (), {
-            "list": custom_roles_list,
-            "list_next": list_next,
-        })()
-
-    def projects():
-        return type("Projects", (), {
-            "serviceAccounts": service_accounts,
-            "roles": project_roles,
-        })()
-
-    def roles():
-        return type("Roles", (), {
-            "list": predefined_roles_list,
-            "list_next": list_next,
-        })()
-
-    iam_client = type("IAMClient", (), {
-        "projects": projects,
-        "roles": roles,
-    })()
 
     common_job_parameters = {
         "PROJECT_ID": TEST_PROJECT_ID,
         "UPDATE_TAG": TEST_UPDATE_TAG,
     }
-
-    # Act
-    cartography.intel.gcp.iam.sync(
-        neo4j_session,
-        iam_client,
-        TEST_PROJECT_ID,
-        TEST_UPDATE_TAG,
-        common_job_parameters,
-    )
-
-    # Assert: Roles
+    with patch.object(
+        cartography.intel.gcp.iam,
+        "get_gcp_service_accounts",
+        return_value=tests.data.gcp.iam.LIST_SERVICE_ACCOUNTS_RESPONSE["accounts"],
+    ), patch.object(
+        cartography.intel.gcp.iam,
+        "get_gcp_roles", 
+        return_value=tests.data.gcp.iam.LIST_ROLES_RESPONSE["roles"],
+    ):
+        cartography.intel.gcp.iam.sync(
+            neo4j_session,
+            None,  
+            TEST_PROJECT_ID,
+            TEST_UPDATE_TAG,
+            common_job_parameters,
+        )
+        
+    # Verify roles
     expected_role_nodes = {
         ("projects/project-123/roles/customRole1",),
         ("roles/editor",),
@@ -196,6 +142,7 @@ def test_sync_gcp_iam(neo4j_session):
         "RESOURCE",
     ) == expected_role_rels
 
+    # Verify service accounts
     expected_sa_nodes = {
         ("112233445566778899",),
         ("998877665544332211",),
