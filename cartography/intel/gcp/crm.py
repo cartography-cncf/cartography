34359b5f2a39d1a53d184eb4e2706bc7ad9f7af0
Kunaal Sikka <19509280+kunaals@users.noreply.github.com>
2025-09-11 15:24:17 -0700

    Migrate GCP project sync to data model

--- cartography/intel/gcp/crm.py
+++ cartography/intel/gcp/crm.py
@@ -1,7 +1,6 @@
 # Google Compute Resource Manager
 # https://cloud.google.com/resource-manager/docs/cloud-platform-resource-hierarchy
 import logging
-from string import Template
 from typing import Dict
 from typing import List
 
@@ -9,6 +8,9 @@ import neo4j
 from googleapiclient.discovery import HttpError
 from googleapiclient.discovery import Resource
 
+from cartography.client.core.tx import load
+from cartography.graph.job import GraphJob
+from cartography.models.gcp.crm import GCPProjectSchema
 from cartography.util import run_cleanup_job
 from cartography.util import timeit
 
@@ -176,68 +178,22 @@ def load_gcp_projects(
     :param gcp_update_tag: The timestamp value to set our new Neo4j nodes with
     :return: Nothing
     """
-    query = """
-    MERGE (project:GCPProject{id:$ProjectId})
-    ON CREATE SET project.firstseen = timestamp()
-    SET project.projectid = $ProjectId,
-    project.projectnumber = $ProjectNumber,
-    project.displayname = $DisplayName,
-    project.lifecyclestate = $LifecycleState,
-    project.lastupdated = $gcp_update_tag
-    """
-
+    # Transform parent hierarchy for relationship creation
     for project in data:
-        neo4j_session.run(
-            query,
-            ProjectId=project["projectId"],
-            ProjectNumber=project["projectNumber"],
-            DisplayName=project.get("name", None),
-            LifecycleState=project.get("lifecycleState", None),
-            gcp_update_tag=gcp_update_tag,
-        )
-        if project.get("parent"):
-            _attach_gcp_project_parent(neo4j_session, project, gcp_update_tag)
+        parent = project.get("parent")
+        project["organization_parent"] = None
+        project["folder_parent"] = None
+        if parent:
+            if parent["type"] == "organization":
+                project["organization_parent"] = f"organizations/{parent['id']}"
+            elif parent["type"] == "folder":
+                project["folder_parent"] = f"folders/{parent['id']}"
 
-
-@timeit
-def _attach_gcp_project_parent(
-    neo4j_session: neo4j.Session,
-    project: Dict,
-    gcp_update_tag: int,
-) -> None:
-    """
-    Attach a project to its respective parent, as in the Resource Hierarchy -
-    https://cloud.google.com/resource-manager/docs/cloud-platform-resource-hierarchy
-    """
-    if project["parent"]["type"] == "organization":
-        parent_label = "GCPOrganization"
-    elif project["parent"]["type"] == "folder":
-        parent_label = "GCPFolder"
-    else:
-        raise NotImplementedError(
-            "Ingestion of GCP {}s as parent nodes is currently not supported. "
-            "Please file an issue at https://github.com/cartography-cncf/cartography/issues.".format(
-                project["parent"]["type"],
-            ),
-        )
-    parent_id = f"{project['parent']['type']}s/{project['parent']['id']}"
-    INGEST_PARENT_TEMPLATE = Template(
-        """
-    MATCH (project:GCPProject{id:$ProjectId})
-
-    MERGE (parent:$parent_label{id:$ParentId})
-    ON CREATE SET parent.firstseen = timestamp()
-
-    MERGE (parent)-[r:RESOURCE]->(project)
-    ON CREATE SET r.firstseen = timestamp()
-    SET r.lastupdated = $gcp_update_tag
-    """,
-    )
-    neo4j_session.run(
-        INGEST_PARENT_TEMPLATE.safe_substitute(parent_label=parent_label),
-        ParentId=parent_id,
-        ProjectId=project["projectId"],
-        gcp_update_tag=gcp_update_tag,
+    load(
+        neo4j_session,
+        GCPProjectSchema(),
+        data,
+        lastupdated=gcp_update_tag,
     )
 
 
@@ -284,10 +240,8 @@ def cleanup_gcp_projects(
     :param common_job_parameters: Parameters to carry to the cleanup job
     :return: Nothing
     """
-    run_cleanup_job(
-        "gcp_crm_project_cleanup.json",
+    GraphJob.from_node_schema(GCPProjectSchema(), common_job_parameters).run(
         neo4j_session,
-        common_job_parameters,
     )
 
 
