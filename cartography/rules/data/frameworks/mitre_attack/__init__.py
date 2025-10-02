ab41596fc1237e9132328f40c8d50b00c6feb616
Alex Chantavy <alex@subimage.io>
2025-10-02 15:01:44 -0700

    restructure
    Signed-off-by: Alex Chantavy <alex@subimage.io>

--- cartography/rules/data/frameworks/mitre_attack/__init__.py
+++ cartography/rules/data/frameworks/mitre_attack/__init__.py
@@ -1,6 +1,6 @@
 # MITRE ATT&CK Framework
-from cartography.rules.data.frameworks.mitre_attack.requirements import (
-    t1190_exploit_public_facing_application,
+from cartography.rules.data.frameworks.mitre_attack.requirements.t1190_exploit_public_facing_application import (
+    t1190,
 )
 from cartography.rules.spec.model import Framework
 
@@ -9,6 +9,6 @@ mitre_attack_framework = Framework(
     name="MITRE ATT&CK",
     description="Comprehensive security assessment framework based on MITRE ATT&CK tactics and techniques",
     version="1.0",
-    requirements=(t1190_exploit_public_facing_application,),
+    requirements=(t1190,),
     source_url="https://attack.mitre.org/",
 )
