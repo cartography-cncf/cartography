5094676daa0bd3a60ed3c2af4420591d830cb37c
Jeremy Chapeau <jeremy@subimage.io>
2025-11-13 15:07:50 -0800

    migrate ontology users mapping
    Signed-off-by: Jeremy Chapeau <jeremy@subimage.io>

--- cartography/models/ontology/mapping/__init__.py
+++ cartography/models/ontology/mapping/__init__.py
@@ -2,15 +2,19 @@ from cartography.models.core.nodes import CartographyNodeSchema
 from cartography.models.ontology.device import DeviceSchema
 from cartography.models.ontology.mapping.data.devices import DEVICES_ONTOLOGY_MAPPING
 from cartography.models.ontology.mapping.data.users import USERS_ONTOLOGY_MAPPING
+from cartography.models.ontology.mapping.data.useraccounts import USERACCOUNTS_ONTOLOGY_MAPPING
 from cartography.models.ontology.mapping.specs import OntologyMapping
 from cartography.models.ontology.user import UserSchema
 
 ONTOLOGY_MAPPING: dict[str, dict[str, OntologyMapping]] = {
     "users": USERS_ONTOLOGY_MAPPING,
     "devices": DEVICES_ONTOLOGY_MAPPING,
+    "useraccounts": USERACCOUNTS_ONTOLOGY_MAPPING,
 }
 
 ONTOLOGY_MODELS: dict[str, type[CartographyNodeSchema]] = {
     "users": UserSchema,
     "devices": DeviceSchema,
+    # Following labels are only semantic labels no independent nodes exist
+    # "useraccounts": None,
 }
