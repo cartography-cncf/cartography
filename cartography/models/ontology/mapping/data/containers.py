c86179686c8d280d8956ad8f6305de60d2ba5348
Jeremy Chapeau <jeremy@subimage.io>
2025-11-26 13:47:05 -0800

    lint
    Signed-off-by: Jeremy Chapeau <jeremy@subimage.io>

--- cartography/models/ontology/mapping/data/containers.py
+++ cartography/models/ontology/mapping/data/containers.py
@@ -43,9 +43,7 @@ kubernetes_mapping = OntologyMapping(
                 OntologyFieldMapping(
                     ontology_field="image_digest", node_field="status_image_sha"
                 ),
-                OntologyFieldMapping(
-                    ontology_field="state", node_field="status_state"
-                ),
+                OntologyFieldMapping(ontology_field="state", node_field="status_state"),
                 # cpu: Not exposed as a direct field in KubernetesContainer node
                 # memory: Not exposed as a direct field in KubernetesContainer node
                 # region: Not exposed as a direct field in KubernetesContainer node
