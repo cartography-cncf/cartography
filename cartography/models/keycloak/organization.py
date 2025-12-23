a126f891ee1a5dbdbfa44835f995f86947f03fc1
Jeremy Chapeau <jeremy@subimage.io>
2025-12-23 11:34:56 -0800

    add tenant label into ontology
    Signed-off-by: Jeremy Chapeau <jeremy@subimage.io>

--- cartography/models/keycloak/organization.py
+++ cartography/models/keycloak/organization.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from cartography.models.core.common import PropertyRef
 from cartography.models.core.nodes import CartographyNodeProperties
 from cartography.models.core.nodes import CartographyNodeSchema
+from cartography.models.core.nodes import ExtraNodeLabels
 from cartography.models.core.relationships import CartographyRelProperties
 from cartography.models.core.relationships import CartographyRelSchema
 from cartography.models.core.relationships import LinkDirection
@@ -114,3 +115,4 @@ class KeycloakOrganizationSchema(CartographyNodeSchema):
             KeycloakOrganizationToIdentityProviderRel(),
         ]
     )
+    extra_node_labels: ExtraNodeLabels = ExtraNodeLabels(["Tenant"])
