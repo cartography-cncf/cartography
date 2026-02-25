7d04c8a3ad329472930358970368b63b0139999c
Kunaal Sikka <kunaal@subimage.io>
2026-02-25 14:17:09 -0800

    test(gitlab): remove internal sync monkeypatches from init test
    Signed-off-by: Kunaal Sikka <kunaal@subimage.io>

--- tests/unit/cartography/intel/gitlab/test_init.py
+++ tests/unit/cartography/intel/gitlab/test_init.py
@@ -17,15 +17,6 @@ def test_start_gitlab_ingestion_raises_http_error_when_org_not_found(monkeypatch
         "cartography.intel.gitlab.organizations.get_single",
         _raise_get_single,
     )
-    # These should not run because org sync fails first.
-    monkeypatch.setattr(
-        "cartography.intel.gitlab.groups.sync_gitlab_groups",
-        lambda *args, **kwargs: pytest.fail("groups sync should not execute"),
-    )
-    monkeypatch.setattr(
-        "cartography.intel.gitlab.projects.sync_gitlab_projects",
-        lambda *args, **kwargs: pytest.fail("projects sync should not execute"),
-    )
 
     config = SimpleNamespace(
         gitlab_token="token",
