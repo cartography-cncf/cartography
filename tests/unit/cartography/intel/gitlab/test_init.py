73c101f97bbdbb30d53b0c590e35edb42b6af18c
Kunaal Sikka <kunaal@subimage.io>
2026-02-25 14:11:55 -0800

    test(gitlab): mock external API call in org error propagation test
    Signed-off-by: Kunaal Sikka <kunaal@subimage.io>

--- tests/unit/cartography/intel/gitlab/test_init.py
+++ tests/unit/cartography/intel/gitlab/test_init.py
@@ -10,12 +10,21 @@ def test_start_gitlab_ingestion_raises_http_error_when_org_not_found(monkeypatch
     response = SimpleNamespace(status_code=404)
     error = requests.exceptions.HTTPError("not found", response=response)
 
-    def _raise_org_sync(*args, **kwargs):
+    def _raise_get_single(*args, **kwargs):
         raise error
 
     monkeypatch.setattr(
-        "cartography.intel.gitlab.organizations.sync_gitlab_organizations",
-        _raise_org_sync,
+        "cartography.intel.gitlab.organizations.get_single",
+        _raise_get_single,
+    )
+    # These should not run because org sync fails first.
+    monkeypatch.setattr(
+        "cartography.intel.gitlab.groups.sync_gitlab_groups",
+        lambda *args, **kwargs: pytest.fail("groups sync should not execute"),
+    )
+    monkeypatch.setattr(
+        "cartography.intel.gitlab.projects.sync_gitlab_projects",
+        lambda *args, **kwargs: pytest.fail("projects sync should not execute"),
     )
 
     config = SimpleNamespace(
