fd965929302bbf3a75eac382147fdbe1632ec06e
Raajhesh Kannaa Chidambaram <495042+raajheshkannaa@users.noreply.github.com>
2026-03-20 21:51:52 -0400

    address review: raw test data, sync() tests, builtin types, lazy logger
    Signed-off-by: Raajhesh Kannaa Chidambaram <495042+raajheshkannaa@users.noreply.github.com>

--- tests/data/aws/apprunner.py
+++ tests/data/aws/apprunner.py
@@ -7,14 +7,30 @@ DESCRIBE_SERVICES = [
         "CreatedAt": "2023-01-01T00:00:00Z",
         "UpdatedAt": "2023-01-02T00:00:00Z",
         "Status": "RUNNING",
-        "Cpu": "1 vCPU",
-        "Memory": "2 GB",
-        "InstanceRoleArn": "arn:aws:iam::123456789012:role/apprunner-instance",
-        "AccessRoleArn": "arn:aws:iam::123456789012:role/apprunner-access",
-        "AutoDeploymentsEnabled": True,
-        "IsPubliclyAccessible": True,
-        "ImageIdentifier": "123456789012.dkr.ecr.us-east-1.amazonaws.com/my-repo:latest",
-        "EgressType": "DEFAULT",
+        "SourceConfiguration": {
+            "ImageRepository": {
+                "ImageIdentifier": "123456789012.dkr.ecr.us-east-1.amazonaws.com/my-repo:latest",
+                "ImageRepositoryType": "ECR",
+                "ImageConfiguration": {},
+            },
+            "AutoDeploymentsEnabled": True,
+            "AuthenticationConfiguration": {
+                "AccessRoleArn": "arn:aws:iam::123456789012:role/apprunner-access",
+            },
+        },
+        "InstanceConfiguration": {
+            "Cpu": "1 vCPU",
+            "Memory": "2 GB",
+            "InstanceRoleArn": "arn:aws:iam::123456789012:role/apprunner-instance",
+        },
+        "NetworkConfiguration": {
+            "EgressConfiguration": {
+                "EgressType": "DEFAULT",
+            },
+            "IngressConfiguration": {
+                "IsPubliclyAccessible": True,
+            },
+        },
     },
     {
         "ServiceName": "my-other-service",
@@ -24,13 +40,29 @@ DESCRIBE_SERVICES = [
         "CreatedAt": "2023-02-01T00:00:00Z",
         "UpdatedAt": "2023-02-02T00:00:00Z",
         "Status": "RUNNING",
-        "Cpu": "2 vCPU",
-        "Memory": "4 GB",
-        "InstanceRoleArn": "arn:aws:iam::123456789012:role/apprunner-instance-2",
-        "AccessRoleArn": "arn:aws:iam::123456789012:role/apprunner-access-2",
-        "AutoDeploymentsEnabled": False,
-        "IsPubliclyAccessible": False,
-        "ImageIdentifier": "123456789012.dkr.ecr.us-east-1.amazonaws.com/other-repo:latest",
-        "EgressType": "VPC",
+        "SourceConfiguration": {
+            "ImageRepository": {
+                "ImageIdentifier": "123456789012.dkr.ecr.us-east-1.amazonaws.com/other-repo:latest",
+                "ImageRepositoryType": "ECR",
+                "ImageConfiguration": {},
+            },
+            "AutoDeploymentsEnabled": False,
+            "AuthenticationConfiguration": {
+                "AccessRoleArn": "arn:aws:iam::123456789012:role/apprunner-access-2",
+            },
+        },
+        "InstanceConfiguration": {
+            "Cpu": "2 vCPU",
+            "Memory": "4 GB",
+            "InstanceRoleArn": "arn:aws:iam::123456789012:role/apprunner-instance-2",
+        },
+        "NetworkConfiguration": {
+            "EgressConfiguration": {
+                "EgressType": "VPC",
+            },
+            "IngressConfiguration": {
+                "IsPubliclyAccessible": False,
+            },
+        },
     },
 ]
