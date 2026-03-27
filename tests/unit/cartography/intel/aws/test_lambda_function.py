827051f37888602d66c3681a26521ff072d2f5f0
Kunaal Sikka <kunaal@subimage.io>
2026-03-27 09:23:41 +0000

    fix(aws): degrade transient sync failures
    Signed-off-by: Kunaal Sikka <kunaal@subimage.io>

---
+++ tests/unit/cartography/intel/aws/test_lambda_function.py
@@ -0,0 +1,112 @@
+from unittest.mock import ANY
+from unittest.mock import MagicMock
+
+import pytest
+from botocore.exceptions import ClientError
+
+from cartography.intel.aws import lambda_function
+from tests.data.aws.lambda_function import LIST_LAMBDA_FUNCTIONS
+
+
+def _client_error(code: str, message: str, status_code: int) -> ClientError:
+    return ClientError(
+        {
+            "Error": {"Code": code, "Message": message},
+            "ResponseMetadata": {"HTTPStatusCode": status_code},
+        },
+        "ListEventSourceMappings",
+    )
+
+
+def test_get_event_source_mappings_raises_transient_failure_after_retry_exhaustion():
+    client = MagicMock()
+    paginator = client.get_paginator.return_value
+    paginator.paginate.side_effect = _client_error(
+        "ServiceException",
+        "An error occurred and the request cannot be processed.",
+        500,
+    )
+
+    with pytest.raises(lambda_function.LambdaSubResourceTransientFailure):
+        lambda_function.get_event_source_mappings(LIST_LAMBDA_FUNCTIONS[0], client)
+
+
+def test_sync_event_source_mappings_skips_failed_function_and_marks_cleanup_unsafe(
+    mocker,
+):
+    load_mappings = mocker.patch(
+        "cartography.intel.aws.lambda_function.load_lambda_event_source_mappings"
+    )
+
+    def _side_effect(lambda_fn, _client):
+        if lambda_fn["FunctionName"] == "sample-function-2":
+            raise lambda_function.LambdaSubResourceTransientFailure("temporary failure")
+        return [{"UUID": lambda_fn["FunctionName"]}]
+
+    mocker.patch(
+        "cartography.intel.aws.lambda_function.get_event_source_mappings",
+        side_effect=_side_effect,
+    )
+
+    cleanup_safe = lambda_function.sync_event_source_mappings(
+        MagicMock(),
+        LIST_LAMBDA_FUNCTIONS[:2],
+        MagicMock(),
+        "123456789012",
+        1,
+    )
+
+    assert cleanup_safe is False
+    load_mappings.assert_called_once_with(
+        ANY,
+        [{"UUID": "sample-function-1"}],
+        "123456789012",
+        1,
+    )
+
+
+def test_sync_skips_only_event_source_mapping_cleanup_after_transient_subresource_failure(
+    mocker,
+):
+    mocker.patch(
+        "cartography.intel.aws.lambda_function.get_lambda_data",
+        return_value=LIST_LAMBDA_FUNCTIONS[:1],
+    )
+    mocker.patch(
+        "cartography.intel.aws.lambda_function.get_lambda_permissions",
+        return_value={
+            LIST_LAMBDA_FUNCTIONS[0]["FunctionArn"]: {
+                "AnonymousAccess": False,
+                "AnonymousActions": [],
+            }
+        },
+    )
+    mocker.patch("cartography.intel.aws.lambda_function.load_lambda_functions")
+    mocker.patch(
+        "cartography.intel.aws.lambda_function.sync_aliases",
+        return_value=True,
+    )
+    mocker.patch(
+        "cartography.intel.aws.lambda_function.sync_event_source_mappings",
+        return_value=False,
+    )
+    mocker.patch("cartography.intel.aws.lambda_function.sync_lambda_layers")
+    cleanup = mocker.patch("cartography.intel.aws.lambda_function.cleanup_lambda")
+
+    lambda_function.sync(
+        MagicMock(),
+        MagicMock(),
+        ["us-east-1"],
+        "123456789012",
+        1,
+        {},
+    )
+
+    cleanup.assert_called_once_with(
+        ANY,
+        {},
+        aliases_cleanup_safe=True,
+        event_source_mappings_cleanup_safe=False,
+        layers_cleanup_safe=True,
+        functions_cleanup_safe=True,
+    )
