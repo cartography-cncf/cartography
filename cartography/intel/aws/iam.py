a8399b8c09bf6a404995aec371d8272351ac2fb2
Sahil Shah <sahilshah2104@gmail.com>
2026-05-20 23:50:38 +0530

    Made requested changes

--- cartography/intel/aws/iam.py
+++ cartography/intel/aws/iam.py
@@ -741,7 +741,8 @@ def load_group_memberships(neo4j_session: neo4j.Session, group_memberships: Dict
     ingest_membership = """
     MATCH (group:AWSGroup{arn: $GroupArn})
     WITH group
-    MATCH (user:AWSUser{arn: $PrincipalArn})
+    MATCH (user:AWSPrincipal{arn: $PrincipalArn})
+    WHERE user:AWSUser OR user:AWSServiceAccount
     MERGE (user)-[r:MEMBER_AWS_GROUP]->(group)
     ON CREATE SET r.firstseen = timestamp()
     SET r.lastupdated = $aws_update_tag
@@ -842,7 +843,8 @@ def load_user_access_keys(
     # To rotate key there is no option in aws. we need to create new and delete existing key that's why rotatedate same as createdate
     # TODO change the node label to reflect that this is a user access key, not an account access key
     ingest_account_key = """
-    MATCH (user:AWSUser{arn: $ARN})
+    MATCH (user:AWSPrincipal{arn: $ARN})
+    WHERE user:AWSUser OR user:AWSServiceAccount
     WITH user
     MERGE (key:AccountAccessKey{accesskeyid: $AccessKeyId})
     ON CREATE SET key.firstseen = timestamp(),
@@ -1326,7 +1328,7 @@ def sync_user_access_keys(
     common_job_parameters: Dict,
 ) -> None:
     logger.info("Syncing IAM user access keys for account '%s'.", current_aws_account_id)
-    query = "MATCH (user:AWSUser)<-[:RESOURCE]-(:AWSAccount{id: $AWS_ACCOUNT_ID}) return user.name as name"
+    query = "MATCH (user:AWSPrincipal)<-[:RESOURCE]-(:AWSAccount{id: $AWS_ACCOUNT_ID}) WHERE user:AWSUser OR user:AWSServiceAccount return user.name as name"
     result = neo4j_session.run(query, AWS_ACCOUNT_ID=current_aws_account_id)
     usernames = [r["name"] for r in result]
     for name in usernames:
@@ -1371,7 +1373,7 @@ def _set_used_state_tx(
     ingest_entity_used = """
     MATCH (:AWSAccount{id: $AWS_ID})-[:RESOURCE]->(n)
     WHERE ()-[:TRUSTS_AWS_PRINCIPAL]->(n) AND n.lastupdated = $update_tag
-    AND labels(n) IN [['AWSUser'], ['AWSGroup']]
+    AND (n:AWSUser OR n:AWSGroup OR n:AWSServiceAccount)
     SET n.isUsed = $isUsed
     """
 
@@ -1386,7 +1388,7 @@ def _set_used_state_tx(
     ingest_entity_unused = """
     MATCH (:AWSAccount{id: $AWS_ID})-[:RESOURCE]->(n)
     WHERE n.isUsed IS NULL AND n.lastupdated = $update_tag
-    AND labels(n) IN [['AWSUser'], ['AWSGroup'], ['AWSRole']]
+    AND (n:AWSUser OR n:AWSGroup OR n:AWSRole OR n:AWSServiceAccount)
     SET n.isUsed = $isUsed
     """
 
